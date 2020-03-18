0ec96ebba41701266fa972c4f7549608ec6b070a
qzhseu <898178704@qq.com>
2020-03-18 14:31:06 -0400

    delete

--- Manager_app/managerapp/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-import os
-
-app=Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24)
-
-from managerapp import routes
--- Manager_app/managerapp/config.py
+++
@@ -1,2 +0,0 @@
-ami_id = 'ami-05174f5df2474e8b4'
-SECRET_KEY = 'ece1779-a2-secretkey'
